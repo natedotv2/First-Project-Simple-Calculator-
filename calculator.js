ab1958df96d1e273cbf0cd03a82d6c17659f7a53
Kayode Akinlade <57946762+sirccay@users.noreply.github.com>
2022-09-11 16:54:25 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,19 +1,44 @@
- // Write a function to display values
- function display(val){
+ // write a function to display values
+function display(val){
     document.getElementById('result').value += val
 
     return val
 }
 
-// Write a function perform arithmetic operations
-function solve(){
+//write a function to perform arithmetic operations
+function solve (){
     let x = document.getElementById('result').value
     let y = eval(x);
     document.getElementById('result').value = y
     return y
 }
 
-// Write a function to clear the result screen after calculating
 function clearScreen() {
     document.getElementById('result').value = ''
+}
+
+function sqrt() {
+    let x = document.getElementById('result').value
+    let y = Math.sqrt('result');
+    document.getElementById('result').value = y
+    return y
+}
+
+function mathcos() {
+    document.getElementById("result").value
+    let y = Math.cos(0 * result / 180);
+    document.getElementById('result').value = y
+    return y
+}
+
+function mathsin() {
+    let x = document.getElementById('result').value
+    let y = x * Math.PI / 180;
+    screen.value = Math.sin(x);
+    document.getElementById('result').value = y
+    return y
+}
+
+function mathlog() {
+    document.getElementById("result").innerHTML = Math.log(1);
 }
