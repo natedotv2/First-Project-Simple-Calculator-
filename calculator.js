1145e05228102750e9cd486eb52565eb7069610f
Nate <89227631+natedotv2@users.noreply.github.com>
2022-09-16 21:01:26 +0100

    changed variable position

--- calculator.js
+++ calculator.js
@@ -1,5 +1,3 @@
-let x = document.getElementById('result').value 
-
 // write a function to display values
 function display(val){
     document.getElementById('result').value += val
@@ -9,6 +7,7 @@ function display(val){
 
 //write a function to perform arithmetic operations
 function solve (){
+    let x = document.getElementById('result').value 
     let y = eval(x);
     document.getElementById('result').value = y
     return y
@@ -19,31 +18,36 @@ function clearScreen() {
 }
 
 function sqrt() {
+    let x = document.getElementById('result').value 
     let y = Math.sqrt(x);
     document.getElementById('result').value = y
     return y
 }
 
 function mathcos() {
+    let x = document.getElementById('result').value 
     let y = Math.cos(x);
     document.getElementById('result').value = y
     return y
 }
 
 function mathsin() {
+    let x = document.getElementById('result').value 
     let y = Math.sin(x);
     document.getElementById('result').value = y
     return y
 }
 
 function Mathtan() {
+    let x = document.getElementById('result').value 
     let y = Math.tan(x);
     document.getElementById('result').value = y
     return y
 }
 
 function mathlog() {
-    let y = Math.log(x)
+    let x = document.getElementById('result').value 
+    let y = Math.log(x);
     document.getElementById("result").value = y
     return y
 }
