036e6f0d6f08dcfa68c6fe3d8390fd73ade2b404
Nate <89227631+natedotv2@users.noreply.github.com>
2022-09-16 20:48:24 +0100

    fixed sin, cos, tan and log

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+let x = document.getElementById('result').value 
+
 // write a function to display values
 function display(val){
     document.getElementById('result').value += val
@@ -7,7 +9,6 @@ function display(val){
 
 //write a function to perform arithmetic operations
 function solve (){
-    let x = document.getElementById('result').value
     let y = eval(x);
     document.getElementById('result').value = y
     return y
@@ -18,27 +19,31 @@ function clearScreen() {
 }
 
 function sqrt() {
-    let x = document.getElementById('result').value
     let y = Math.sqrt(x);
     document.getElementById('result').value = y
     return y
 }
 
 function mathcos() {
-    document.getElementById("result").value
-    let y = Math.cos(0 * result / 180);
+    let y = Math.cos(x);
     document.getElementById('result').value = y
     return y
 }
 
 function mathsin() {
-    let x = document.getElementById('result').value
-    let y = x * Math.PI / 180;
-    screen.value = Math.sin(x);
+    let y = Math.sin(x);
+    document.getElementById('result').value = y
+    return y
+}
+
+function Mathtan() {
+    let y = Math.tan(x);
     document.getElementById('result').value = y
     return y
 }
 
 function mathlog() {
-    document.getElementById("result").innerHTML = Math.log(1);
+    let y = Math.log(x)
+    document.getElementById("result").value = y
+    return y
 }
